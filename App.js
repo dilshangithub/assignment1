e0cac29a6e0a71974c67a454865599254a749c4e
dilshangithub <uggdilshan@gmail.com>
2022-08-31 01:32:33 +0530

    #

--- App.js
+++ App.js
@@ -1,129 +1,122 @@
-// import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 
-import React from 'react';
-import {Node} from 'react';
+import * as React from 'react';
+import {Button, View, Text} from 'react-native';
 
-import World from './src/World';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import KeepAwake from 'react-native-keep-awake';
 
-// import * as React from 'react';
-// import {Button, View, Text} from 'react-native';
+import WelcomScreen from './screens/welcomeScreen';
+import HomeScreen from './screens/homeScreen';
+import GameScreen from './screens/gameScreen';
+import LoginScreen from './screens/loginScreen';
+import SignupScreen from './screens/signupScreen';
+import ForgotPasswordScreen from './screens/forgotpasswordScreen';
 
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
-// import KeepAwake from 'react-native-keep-awake';
+const Stack = createStackNavigator();
 
-// import WelcomScreen from './screens/welcomeScreen';
-// import HomeScreen from './screens/homeScreen';
-// import GameScreen from './screens/gameScreen';
-// import LoginScreen from './screens/loginScreen';
-// import SignupScreen from './screens/signupScreen';
-// import ForgotPasswordScreen from './screens/forgotpasswordScreen';
+KeepAwake.activate();
 
-// const Stack = createStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        // initialRouteName={'SignupScreen'}
+      >
+        <Stack.Screen
+          name="WelcomScreen"
+          component={WelcomScreen}
+          options={{
+            title: ' ', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
 
-// KeepAwake.activate();
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
 
-// const App: () => Node = () => {
-  const App = () => {
-  return <World />;
-  // return (
-  //   <NavigationContainer>
-  //     <Stack.Navigator
-  //       screenOptions={{
-  //         headerShown: false,
-  //       }}
-  //       // initialRouteName={'SignupScreen'}
-  //     >
-  //       <Stack.Screen
-  //         name="WelcomScreen"
-  //         component={WelcomScreen}
-  //         options={{
-  //           title: ' ', //Set Header Title
-  //           headerStyle: {
-  //             backgroundColor: '#f4511e',
-  //           },
-  //           headerTintColor: '#fff',
-  //           headerTitleStyle: {
-  //             fontWeight: 'bold',
-  //           },
-  //         }}
-  //       />
+        <Stack.Screen
+          name="GameScreen"
+          component={GameScreen}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
 
-  //       <Stack.Screen
-  //         name="HomeScreen"
-  //         component={HomeScreen}
-  //         options={{
-  //           title: '',
-  //           headerStyle: {
-  //             backgroundColor: '#f4511e',
-  //           },
-  //           headerTintColor: '#fff',
-  //           headerTitleStyle: {
-  //             fontWeight: 'bold',
-  //           },
-  //         }}
-  //       />
-
-  //       <Stack.Screen
-  //         name="GameScreen"
-  //         component={GameScreen}
-  //         options={{
-  //           title: '',
-  //           headerStyle: {
-  //             backgroundColor: '#f4511e',
-  //           },
-  //           headerTintColor: '#fff',
-  //           headerTitleStyle: {
-  //             fontWeight: 'bold',
-  //           },
-  //         }}
-  //       />
-
-  //       <Stack.Screen
-  //         name="LoginScreen"
-  //         component={LoginScreen}
-  //         options={{
-  //           title: '',
-  //           headerStyle: {
-  //             backgroundColor: '#f4511e',
-  //           },
-  //           headerTintColor: '#fff',
-  //           headerTitleStyle: {
-  //             fontWeight: 'bold',
-  //           },
-  //         }}
-  //       />
-  //       <Stack.Screen
-  //         name="SignupScreen"
-  //         component={SignupScreen}
-  //         options={{
-  //           title: '',
-  //           headerStyle: {
-  //             backgroundColor: '#f4511e',
-  //           },
-  //           headerTintColor: '#fff',
-  //           headerTitleStyle: {
-  //             fontWeight: 'bold',
-  //           },
-  //         }}
-  //       />
-  //       <Stack.Screen
-  //         name="ForgotPasswordScreen"
-  //         component={ForgotPasswordScreen}
-  //         options={{
-  //           title: '',
-  //           headerStyle: {
-  //             backgroundColor: '#f4511e',
-  //           },
-  //           headerTintColor: '#fff',
-  //           headerTitleStyle: {
-  //             fontWeight: 'bold',
-  //           },
-  //         }}
-  //       />
-  //     </Stack.Navigator>
-  //   </NavigationContainer>
-  // );
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="SignupScreen"
+          component={SignupScreen}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="ForgotPasswordScreen"
+          component={ForgotPasswordScreen}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- screens/gameScreen.js
+++ screens/gameScreen.js
@@ -2,24 +2,13 @@ import React, { useEffect } from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
 import KeepAwake from 'react-native-keep-awake';
 import SuccessButton from '../components/successbutton';
+import World from '../src/World';
 
 
 KeepAwake.activate();
 
 const GameScreen = ({ navigation }) => {
-    useEffect(() => {
-    }, []);
-   
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ flex: 1, backgroundColor: 'green' }}>
-          <View style={{ flex: 1 }}>
-            
-
-          </View>
-        </View>
-      </SafeAreaView>
-    );
+  return <World />;
   };
    
   export default GameScreen;
